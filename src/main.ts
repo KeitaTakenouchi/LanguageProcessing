cad109a568afaa087d37ac5927f666b88dd89bb1
Takenouchi Keita <t.keita.osaka@gmail.com>
2017-09-27 00:12:06 +0900

    Narrow a scope of a require variable.

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,10 @@
-import fs = require('fs');
+
 import { Scanner } from './parser/scanner';
 import { Token } from './parser/token';
 
 export class Main {
     public static run(): void {
+        let fs = require('fs');
         let filename: string = "../sample_programs/c.tip"
         let body: string = fs.readFileSync(filename, "UTF-8");
         let scanner = new Scanner(body);
