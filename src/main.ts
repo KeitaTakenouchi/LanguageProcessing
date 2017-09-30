6e4f4492880c83ab28e17f6876a3d88960ce6f43
Takenouchi Keita <t.keita.osaka@gmail.com>
2017-09-30 12:49:38 +0900

    Refactoring with tslint/

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ import { Token } from "./parser/token";
 export class Main {
     public static run(): void {
         const fs = require("fs");
-        const filename: string = "../sample_programs/c.tip"
+        const filename: string = "../sample_programs/c.tip";
         const body: string = fs.readFileSync(filename, "UTF-8");
         const scanner = new Scanner(body);
 
--- src/parser/ast/astnodes/programnode.ts
+++
@@ -1,2 +0,0 @@
-import { IASTNode } from "../astnode";
-
